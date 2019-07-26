719821b59f8338fe522ad75fdafda116683eba15
Daniel Delgado <danielalejandro.ds@gmail.com>
2019-07-26 09:53:15 +0200

    Iteration 2 completed

---
+++ starter_code/elevator.js
@@ -0,0 +1,44 @@
+class Elevator {
+  constructor(){
+    this.floor      = 0;
+    this.MAXFLOOR   = 10;
+    this.requests   = [];
+    this.direction ="up"
+    this.timer =""
+  }
+
+  start() {
+    this.timer =setInterval(()=>this.update(),1000)
+   }
+  stop() {
+    clearInterval(this.timer)
+   }
+  update() { 
+    this.log()
+  }
+  _passengersEnter() { }
+  _passengersLeave() { }
+  floorUp() { 
+    if(this.floor == this.MAXFLOOR){
+      console.log("Imposiburu!")      
+    }else{
+      this.floor += 1
+    }
+    
+  }
+  floorDown() {
+    if(this.floor == 0){
+      console.log("Imposiburu! for now");
+      
+    }else{
+      this.floor -= 1
+    }
+    
+   }
+  call() { }
+  log() {
+    console.log("Direction: " + this.direction + " | Floor: " + this.floor);    
+   }
+}
+
+module.exports = Elevator;
---
+++ starter_code/index.js
@@ -0,0 +1,6 @@
+const Elevator = require('./elevator.js');
+const person = require('./person')
+
+let elevator1 = new Elevator()
+elevator1.update()
+elevator1.start()
